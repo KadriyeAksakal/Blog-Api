ea4f7f3a68bba98e08638127f05ad5eca92c660b
kadriyeaksakal <kadriyeaksakal.ka@gmail.com>
2022-05-14 00:19:14 +0300

    [ENHANCE] contact content with component

--- src/api/contact-content/controllers/contact-content.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  contact-content controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::contact-content.contact-content');
--- src/api/contact-content/services/contact-content.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * contact-content service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::contact-content.contact-content');
